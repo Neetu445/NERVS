a492c3e2c864bd0df8ae7fd2ecb1a60d218e0edf
Neetu445 <neetupathak449@gmail.com>
2026-01-30 23:55:32 +0530

    add baseline metrics for countries

--- core/data.py
+++ core/data.py
@@ -1,1 +1,26 @@
-#core/data.py
+countries = {"India":{"economic": 70,
+                      "defence":75,
+                      "healthcare":65,
+                      "cyber":60,
+                      "demographic":68,
+                      "energy":62,
+                      "debt":55},
+             "USA":{
+                     "economic": 78,
+                      "defence":85,
+                      "healthcare":72,
+                      "cyber":80,
+                      "demographic":70,
+                      "energy":75,
+                      "debt":55,
+             },
+             "China":{"economic": 76,
+                      "defence":82,
+                      "healthcare":70,
+                      "cyber":78,
+                      "demographic":65,
+                      "energy":72,
+                      "debt":58
+                     }
+            }
+               
