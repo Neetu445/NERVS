82c923cc5f45053dede7aecebb2e1ba20caf322b
Neetu445 <neetupathak449@gmail.com>
2026-01-30 23:59:52 +0530

    add resilience calculation model

--- core/model.py
+++ core/model.py
@@ -1,1 +1,8 @@
+def calculate_resilience(metrics: dict) -> float:
+  """calculates average resilience score from all metrics"""
+  total = 0
+  for value in matrics.values():
+    total += value
 
+score = total/len(metrics)
+return round(score,2)
