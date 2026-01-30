f868e6ae6a045e07018c682de67f76c55283a285
Neetu445 <neetupathak449@gmail.com>
2026-01-30 15:07:02 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(page_title="NERVS", layout="wide")
+
+st.title("NERVS")
+st.subheader("National Elastic Resilience & Vulnerability System")
+
+st.write("""NERVS is an AI-driven stress-test engine that models how contries absorb,
+amplify, and recover from global shocks.""")
