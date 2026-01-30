64cb7b3b7273d204591d6fd34a4cec5774792ea7
Neetu445 <neetupathak449@gmail.com>
2026-01-30 15:29:32 +0530

    Add country input slider

--- app.py
+++ app.py
@@ -6,3 +6,22 @@ st.subheader("National Elastic Resilience & Vulnerability System")
 
 st.write("""NERVS is an AI-driven stress-test engine that models how contries absorb,
 amplify, and recover from global shocks.""")
+
+st.header("Input Country Stress Indicators")
+
+countries=["India", "China","USA", "Japan", "Russia"]
+
+indicators = ["Economic Stability,
+              "Defence & Security", "Healthcare Readiness", "Cyber Infrastructure", "Energy Security"]
+data ={}
+
+for country in countries:
+  st.subheader(country)
+  data[country] = {}
+  for indicator in indicators:
+    value = st.slider(f"{indicator} ({country})",
+                      min-value=1,
+                      max_value=10,
+                      value= 5,
+                      key=f"{country}_{indicator}")
+    data[country][indicator] = value
